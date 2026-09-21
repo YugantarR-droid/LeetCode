149a48ba76096d162e71d5ab557714f53b4558b1
YugantarR-droid <yugantarr190@gmail.com>
2026-09-21 07:09:36 +0530

    Time: 27 ms (6.76%) | Memory: 75.9 MB (5.13%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getIndex(int element,vector<int> inorder){
+        for(int i=0;i<inorder.size();i++){
+            if(inorder[i]==element){
+                return i;
+            }
+        }
+        return -1;
+    }
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder,
+                    int inorderStart, int inorderEnd, int& preorderIndex) {
+        if (preorderIndex == preorder.size())
+            return NULL;
+
+        if (inorderStart > inorderEnd)
+            return NULL;
+
+        int preorderElement = preorder[preorderIndex];
+        preorderIndex++;
+        int searchedIndexInorder = getIndex(preorderElement,inorder);
+        int inorderElement = inorder[searchedIndexInorder];
+
+        TreeNode* root = new TreeNode(inorderElement);
+
+        root->left = solve(preorder, inorder, inorderStart,
+                           searchedIndexInorder - 1, preorderIndex);
+        
+        root->right = solve(preorder, inorder, searchedIndexInorder + 1,
+                            inorderEnd, preorderIndex);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int inorderStart = 0;
+        int inorderEnd = inorder.size() - 1;
+        int preorderIndex = 0;
+        return solve(preorder, inorder, inorderStart, inorderEnd,
+                     preorderIndex);
+    }
+};
