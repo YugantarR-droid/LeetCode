858de60f96fa70e095ab81379253468912ec341b
YugantarR-droid <yugantarr190@gmail.com>
2026-09-22 00:56:37 +0530

    Time: 0 ms (100.00%) | Memory: 65 MB (61.58%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -11,27 +11,48 @@
  */
 class Solution {
 public:
-    int findSum(TreeNode* root, int low, int high) {
-        if(root == NULL)
+    // int findSum(TreeNode* root, int low, int high) {
+    //     if(root == NULL)
+    //         return 0;
+
+    //     int sum = 0;
+
+    //     if(root->val >= low && root->val <= high) {
+    //         sum += root->val + findSum(root->left,low,high)+findSum(root->right,low,high);
+    //     }
+
+    //     else if(root->val<low){
+    //         sum += findSum(root->right, low, high);
+    //     }
+    //     else{
+    //         sum += findSum(root->left, low, high);
+    //     }
+
+    //     return sum;
+    // }
+
+    int rangeSumBST(TreeNode* root, int low, int high) {
+         if(root == NULL)
             return 0;
 
+        //sum ko 0 se initialize kro
         int sum = 0;
 
+        //afar within range valuse hai to add kro aur left right me search kro
         if(root->val >= low && root->val <= high) {
-            sum += root->val + findSum(root->left,low,high)+findSum(root->right,low,high);
+            sum += root->val + rangeSumBST(root->left,low,high)+rangeSumBST(root->right,low,high);
         }
 
+        //agar current value low se choti hai to left part ignore kro aur right me search kro
         else if(root->val<low){
-            sum += findSum(root->right, low, high);
+            sum += rangeSumBST(root->right, low, high);
         }
+        //agar current value high se badi hai to right part ignore kro aur left me search kro
         else{
-            sum += findSum(root->left, low, high);
+            sum += rangeSumBST(root->left, low, high);
         }
 
         return sum;
-    }
-
-    int rangeSumBST(TreeNode* root, int low, int high) {
-        return findSum(root, low, high);
+    
     }
 };
