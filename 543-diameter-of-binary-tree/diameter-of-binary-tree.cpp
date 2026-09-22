c0d3e9c8a66fdd99fbc7a67931aeedb83cb718b0
YugantarR-droid <yugantarr190@gmail.com>
2026-09-22 20:34:00 +0530

    Time: 280 ms (5.90%) | Memory: 24.1 MB (5.10%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -6,30 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if (root == NULL)
-            return 0;
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)
+        return 0;
 
-        int leftAns = height(root->left) + 1;
-        int rightAns = height(root->right) + 1;
+        int leftAns = maxDepth(root->left)+1;
+        int rightAns = maxDepth(root->right)+1;
 
-        return max(leftAns, rightAns);
+        return max(leftAns,rightAns);
+
+
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        if (root == NULL)
-            return 0;
+        if(root==NULL)
+        return 0;
 
-        int opt1 = height(root->left) + height(root->right);
+        int opt1 = maxDepth(root->left) + maxDepth(root->right);
         int opt2 = diameterOfBinaryTree(root->left);
         int opt3 = diameterOfBinaryTree(root->right);
 
         return max(opt1, max(opt2, opt3));
     }
 };
-
