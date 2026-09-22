abd1e8095c8a19deeb1c764b5078f8923b283921
YugantarR-droid <yugantarr190@gmail.com>
2026-09-22 08:57:10 +0530

    Time: 4 ms (3.20%) | Memory: 22 MB (44.29%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,36 +1,38 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool solve(TreeNode* root,long long lb,long long ub){
-        if(root==NULL)
-        return true;
+    long long findMin(TreeNode* root) {
+        if(root == NULL)
+            return LLONG_MAX;
 
-        bool currentNode;
-        if(root->val>lb && root->val<ub){
-            currentNode = true;
-        }else{
-            currentNode = false;
-        }
+        long long leftMin = findMin(root->left);
+        long long rightMin = findMin(root->right);
 
-        bool leftAns = solve(root->left,lb,root->val);
-        bool rightAns = solve(root->right,root->val,ub);
-
-        return currentNode && leftAns && rightAns;
+        return min((long long)root->val, min(leftMin, rightMin));
     }
-    bool isValidBST(TreeNode* root) {
-        long long lb = LONG_MIN;
-        long long ub = LONG_MAX;
 
-        return solve(root,lb,ub);
+    long long findMax(TreeNode* root) {
+        if(root == NULL)
+            return LLONG_MIN;
+
+        long long leftMax = findMax(root->left);
+        long long rightMax = findMax(root->right);
+
+        return max((long long)root->val, max(leftMax, rightMax));
+    }
+
+    bool isValidBST(TreeNode* root) {
+        if(root == NULL)
+            return true;
+
+        long long leftTree = findMax(root->left);
+        long long rightTree = findMin(root->right);
+
+        if(root->val <= leftTree || root->val >= rightTree)
+            return false;
+
+        bool leftAns = isValidBST(root->left);
+        bool rightAns = isValidBST(root->right);
+
+        return leftAns && rightAns;
     }
 };
