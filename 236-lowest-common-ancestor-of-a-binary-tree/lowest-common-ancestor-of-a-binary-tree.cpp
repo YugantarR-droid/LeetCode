e2da3d6f2414fec41f600f0d08c11be70210c676
YugantarR-droid <yugantarr190@gmail.com>
2026-09-23 19:09:26 +0530

    Time: 58 ms (84.24%) | Memory: 44.1 MB (12.55%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -13,21 +13,30 @@ public:
         if(root == NULL)
             return NULL;
 
+
+        //agar pehli node p mil gyi to aage mt dhundo return krdo
         if(root == p)
             return p;
-
+        //agar pehli node q mil gyi to aage mt dhundo return krdo
         if(root == q)
             return q;
 
+        //left se ans nikalo
         TreeNode* leftAns = lowestCommonAncestor(root->left ,p ,q);
+        //right se ans nikalo
         TreeNode* rightAns = lowestCommonAncestor(root->right, p,q);
 
+        //3 case banege
+
+        //1) left se non null aya ho aur right se non null aya ho to current node he ans hoga
         if(leftAns != NULL && rightAns != NULL)
         return root;
 
+        //2) left se non null aya ho aur right se  null aya ho to left node he ans hoga
         else if(leftAns!= NULL && rightAns == NULL)
         return leftAns;
 
+        //2) right se non null aya ho aur left se  null aya ho to right node he ans hoga
         else
         return rightAns;
     }
