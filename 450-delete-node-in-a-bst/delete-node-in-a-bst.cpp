7629db401663362244ac9e68b3bd6505a4bb9e8f
YugantarR-droid <yugantarr190@gmail.com>
2026-09-23 22:02:13 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (14.70%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -6,58 +6,59 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int getMax(TreeNode* root) {
-        while (root->right != NULL) {
-            root = root->right;
-        }
-        return root->val;
+    int findMax(TreeNode* root) {
+    while(root->right != NULL) {
+        root = root->right;
     }
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if (root == NULL)
-            return NULL;
 
-        if (root->val == key) {
-            // 4 cases
-            // 1) leaf node
-            if (root->left == NULL && root->right == NULL) {
+    return root->val;
+}
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL)
+        return NULL;
+
+        if(root->val==key){
+            //4 cases
+
+            //it is a lead node
+            if(root->left == NULL && root->right==NULL){
                 TreeNode* temp = root;
                 delete temp;
                 return NULL;
             }
-
-            // 2) has left child
-            if (root->left != NULL && root->right == NULL) {
+            
+            //it is root with left child;
+            if(root->left!=NULL && root->right==NULL){
                 TreeNode* temp = root;
                 TreeNode* child = root->left;
                 delete temp;
                 return child;
             }
-            // 3) has right child
-            if (root->left == NULL && root->right != NULL) {
+            //it is root with right child;
+            if(root->left==NULL && root->right!=NULL){
                 TreeNode* temp = root;
                 TreeNode* child = root->right;
                 delete temp;
                 return child;
             }
-            //4) has both childs
-            if (root->left != NULL && root->right != NULL) {
-                // get max element in left subtree
-                int element = getMax(root->left);
-                root->val = element;
-                root->left = deleteNode(root->left, element);
+            //root with both child
+            if(root->left!=NULL && root->right!=NULL){
+                int maxElement = findMax(root->left);
+                root->val=maxElement;
+                root->left = deleteNode(root->left,maxElement);
+                return root;
             }
-        } else if(key<root->val){
-            // left 
-           root->left = deleteNode(root->left, key);
-        } else{
-            //right
-           root->right = deleteNode(root->right, key);
+        }
+        else if(key>root->val){
+            root->right = deleteNode(root->right,key);
+        }
+        else{
+            root->left = deleteNode(root->left,key);
         }
         return root;
     }
