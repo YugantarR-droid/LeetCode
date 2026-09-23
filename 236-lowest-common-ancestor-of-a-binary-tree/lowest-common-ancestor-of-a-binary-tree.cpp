790d882cb25eb93c6a26a78003081ff1928df19d
YugantarR-droid <yugantarr190@gmail.com>
2026-09-23 19:05:44 +0530

    Time: 56 ms (89.81%) | Memory: 43.9 MB (78.49%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,34 +10,25 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL)
-        return NULL;
+        if(root == NULL)
+            return NULL;
 
-        if(root==p)
-        return p;
+        if(root == p)
+            return p;
 
-        if(root==q)
-        return q;
+        if(root == q)
+            return q;
 
-        //search in left
-        TreeNode* leftAns = lowestCommonAncestor(root->left,p,q);
+        TreeNode* leftAns = lowestCommonAncestor(root->left ,p ,q);
+        TreeNode* rightAns = lowestCommonAncestor(root->right, p,q);
 
-        //search in right
-        TreeNode* rightAns = lowestCommonAncestor(root->right,p,q);
-
-        // 3 cases
-        //1) both side is not NULL then root in common ancestor
-        if(leftAns!=NULL && rightAns!=NULL)
+        if(leftAns != NULL && rightAns != NULL)
         return root;
 
-        //2) left side is  NULL right side is not NULL
-        else if(leftAns==NULL && rightAns!=NULL)
+        else if(leftAns!= NULL && rightAns == NULL)
+        return leftAns;
+
+        else
         return rightAns;
-
-        //3) right side is not NULL left side is NULL
-        else return leftAns;
-
-
-
     }
 };
